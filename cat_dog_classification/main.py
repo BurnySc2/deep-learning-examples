86a80582d90e021c6b5b599e02f1f92a2bf78880
burny <gamingburny@gmail.com>
2023-12-22 21:22:22 +0100

    Add packages

---
+++ cat_dog_classification/main.py
@@ -0,0 +1,9 @@
+"""
+https://www.kaggle.com/c/dogs-vs-cats/overview
+Download dataset
+Build data loader
+Build / define model (?)
+Train model (if exists locally: load instead)
+Save model to file
+Get a test image and classify
+"""
