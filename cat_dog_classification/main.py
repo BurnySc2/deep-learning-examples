fc9f8ebbbe5d3e98dfb7f69658a0a09a1874c8d2
burny <gamingburny@gmail.com>
2023-12-27 02:37:57 +0100

    Add first model example

--- cat_dog_classification/main.py
+++
@@ -1,9 +0,0 @@
-"""
-https://www.kaggle.com/c/dogs-vs-cats/overview
-Download dataset
-Build data loader
-Build / define model (?)
-Train model (if exists locally: load instead)
-Save model to file
-Get a test image and classify
-"""
